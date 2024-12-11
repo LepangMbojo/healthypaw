4e0188cb56d7d4a8247be7eeb819a6b89884e2dc
M. Khalid <166613766+LepangMbojo@users.noreply.github.com>
2024-12-11 20:29:27 +0800

    Update Opik.java

--- Opik.java
+++ Opik.java
@@ -1,7 +1,7 @@
 public class Opik {
     // ini saya lagi ngedit file opik java , diambna saya juga sanzgats  senang mengedti file ini
     
-    voif meki(){
+    voif coba(){
         System.out.println("kkk");
     }
 }
